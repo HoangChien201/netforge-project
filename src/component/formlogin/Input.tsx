4ab2cdf7547b0fdcf41b59cb802f0cff3bd25e5a
Hoang Chien <130383912+HoangChien201@users.noreply.github.com>
2024-08-26 16:21:04 +0700

    fix input

--- src/component/formlogin/Input.tsx
+++ src/component/formlogin/Input.tsx
@@ -1,7 +1,9 @@
-import { View, Text, StyleSheet, TextInput } from "react-native"
-import React, { useState } from "react"
+import { View, Text, StyleSheet, TextInput, Animated  } from "react-native"
+import React, { useCallback, useEffect, useState } from "react"
 import { Image } from "react-native"
-import Iconbutton from "./Iconbutton"
+import Iconbutton from "../form/Iconbutton"
+import { COLOR } from "../../constant/color"
+
 
 type props = {
     label: string,
@@ -13,13 +15,42 @@ type props = {
     iconP?: boolean,
     iconPass?: boolean,
     invalid?:boolean,
-    labels?:string
+    errorMessage?:string | null
 
 }
 
-const Input = (props: props) => {
-    const { labels,label, onchangText, value, requireField, password, iconE, iconP, iconPass, invalid } = props;
+const InputLogin = (props: props) => {
+    const { label, onchangText, value, requireField, password, iconE, iconP, iconPass, invalid, errorMessage} = props;
     const [hidePassword, setHidePassword] = useState<boolean | undefined>(password)
+    const [showInvalid, setShowInvalid] = useState(invalid);
+    const [isFocused, setIsFocused] = useState(false);
+    const ref = React.useRef<TextInput>(null);
+    
+
+    useEffect(() => {
+        setShowInvalid(invalid);
+        
+    }, [invalid]);
+    
+
+    const handleFocus = () => {
+        if (ref.current) {
+            setIsFocused(true)
+            ref.current.setNativeProps({
+            style: { borderColor:COLOR.PrimaryColor, borderBottomWidth: 1 } // Đặt màu border khi focus
+          });
+        }
+       
+      };
+    
+      const handleBlur = () => {
+        if (ref.current) {
+            setIsFocused(false)
+            ref.current.setNativeProps({
+            style: { borderColor: '#DDDDDD', borderBottomWidth: 1 } // Xóa màu border khi blur
+          });
+        }
+      };
 
     function Secure() {
         if (hidePassword) {
@@ -31,22 +62,27 @@ const Input = (props: props) => {
 
     const EyePass = () =>{
         return(
-            hidePassword ? <View style={{position:"absolute",end:10,top:20}}>
+            hidePassword ? <View style={{position:"absolute",end:10,top:45}}>
                 <Iconbutton name='eye-slash' size={20} color='#DCDCDC' onPress={Secure}/>
             </View>:
-             <View style={{position:"absolute",end:10,top:20}}>
+             <View style={{position:"absolute",end:10,top:45}}>
              <Iconbutton name='eye' size={20} color='#DCDCDC' onPress={Secure}/>
          </View>
         )
     }
-
+   
     const onchantext = (value: string) => {
-        onchangText(value)
+        onchangText(value)  
+       handleFocus();
+       
+        
     }
+    
     return (
-        <View style={[invalid && {margin:0},{margin:9}]}>
-            {/* <Text style={[styles.label]}>{label} {requireField && <Text style={{color:"#C30052"}}>*</Text>}</Text> */}
-            <TextInput placeholder={"Nhập"+" "+labels} style={[styles.input,invalid && styles.validation]} secureTextEntry={hidePassword} value={value} onChangeText={onchantext} />
+        
+        <View  style={[invalid && {margin:0},{margin:9}]}>
+            <Text  style={[styles.label]}>{label} {requireField && <Text style={{color:"#C30052"}}>*</Text>}</Text>
+            <TextInput style={{borderBottomColor: showInvalid && 'red'}}  onFocus={handleFocus} onBlur={handleBlur} ref={ref} placeholder={" "+label} style={[styles.input,showInvalid && styles.validation]} secureTextEntry={hidePassword} value={value} onChangeText={onchantext} />
             {iconE && <Image style={styles.iconMail} source={require('../../media/icon/Mail.png')} />}
             {iconPass && <Image style={styles.iconMail} source={require('../../media/icon/Password.png')} />}
             {iconP && <Image style={{
@@ -60,45 +96,47 @@ const Input = (props: props) => {
                 password && <EyePass/>
             }
             {
-                invalid && <Text style={{fontSize:10,position:"absolute",bottom:-15,left:10,color:"red",fontWeight:"400", fontFamily: "poppins"
-                }}> {label} !</Text>
+                
+                
+                showInvalid && errorMessage &&<Text style={{fontSize:12,position:"absolute",bottom:-20,left:10,color:"red",fontWeight:"400", fontFamily: "poppins"
+                }}> {errorMessage} </Text>
             }
          
         </View>
     )
 }
-export default Input
+export default InputLogin
 
 const styles = StyleSheet.create({
     input: {
-        borderRadius: 6,
-        borderWidth: 1,
+        borderBottomWidth: 1,
         borderColor: '#DDDDDD',
-        height: 60,
+        height: 50,
         paddingLeft: 35,
-        backgroundColor:'#F5F5F5'
+
     },
     label: {
-        width: 80,
+        width: 200,
         height: 21,
         fontFamily: "poppins",
         fontWeight: "400",
         lineHeight: 21,
         fontSize: 14,
         letterSpacing: 0.12,
-        marginVertical: 5
+        marginVertical: 5,
+        marginLeft:10,
+        color:"black"
     },
     iconMail: {
         position: 'absolute',
-        top: 19,
+        top: 45,
         start: 10
     },
-  validation:{
-        borderRadius: 6,
+    validation:{
+      
         borderColor:"#C30052",
-        borderWidth:1,
-        height: 60,
+        borderBottomWidth: 1,
+        height: 50,
         paddingLeft: 35,
     }
 })
-  
